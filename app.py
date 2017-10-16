22657867a2420b4db867d61b598a5763b96b699f
Phelipe Silva <phelipediv@gmail.com>
2017-10-16 15:36:27 -0200

    CRUD

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, render_template, redirect, url_for
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 from json import dumps
 import json
 
 app = Flask(__name__)
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('ID')
+parser.add_argument('Name')
+parser.add_argument('CountryCode')
+parser.add_argument('District')
+parser.add_argument('Population')
 
 #dialect+driver://username:password@host:port/database
 engine = create_engine("mysql+mysqlconnector://root:@127.0.0.1:3306/world")
@@ -14,23 +21,46 @@ class paises(Resource):
     def get(self):
         conn = engine.connect()
         query = conn.execute("SELECT * FROM country")
-        return {"paises" : [i[0] for i in query.cursor.fetchall()]}
+        return {"paises" : [dict(zip(tuple(query.keys()), i))for i in query.cursor]}
 
 class cidades(Resource):
+    def get(self):
+        conn = engine.connect()
+        query = conn.execute("SELECT * FROM city")
+        return {"cidades" : [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+    def post(self):
+        args = parser.parse_args()
+        conn = engine.connect()
+        query = conn.execute("INSERT INTO city(ID, Name, CountryCode, District, Population)\
+        VALUES(null, '%s', '%s', '%s', %d)" %(args['Name'],args['CountryCode'],args['District'],int(args['Population'])))
+        return args
+    def patch(self):
+        args = parser.parse_args()
+        conn = engine.connect()
+        query = conn.execute("UPDATE city SET Name='%s', CountryCode='%s', District='%s', Population='%d' WHERE ID = %d"
+        %( args['Name'], args['CountryCode'], args['District'], int(args['Population']), int(args['ID'])))
+        return "UPDATED"
+
+class cidades_pais(Resource):
     def get(self, code):
         conn = engine.connect()
         query = conn.execute("SELECT * FROM city WHERE CountryCode = '%s'" %code.upper())
         return {"cidades" : [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+    def delete(self, code):
+        conn = engine.connect()
+        query = conn.execute("DELETE FROM city WHERE ID = %d" %(int(code)))
+        return "DELETED"
 
 class linguas(Resource):
-    def get(self, code):
+    def get(self):
         conn = engine.connect()
-        query = conn.execute("SELECT * FROM countrylanguage WHERE CountryCode = '%s'" %code.upper)
+        query = conn.execute("SELECT * FROM countrylanguage")
         return {"linguas" : [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
 
-
 api.add_resource(paises, "/paises")
-api.add_resource(cidades, "/cidades/<string:code>")
+api.add_resource(cidades, "/cidades")
+api.add_resource(cidades_pais, "/cidades/<code>")
+api.add_resource(linguas, "/linguas")
 
 if __name__ == '__main__':
-    app.run()
+    app.run("127.0.0.1", port=4000, debug=True)
--- site.py
+++ site.py
@@ -1,20 +1,71 @@
 from flask import Flask, request, render_template, redirect, url_for
 import json
 import urllib2
+import requests
 
 app = Flask(__name__)
-api_url="http://127.0.0.1:5000/"
+api_url="http://127.0.0.1:4000/"
 
 @app.route("/")
 def home():
     paises = json.loads(urllib2.urlopen(api_url+"paises").read())
     return render_template("index.html", paises = paises['paises'])
 
-@app.route("/cidades/", methods=["POST", "GET"])
+@app.route("/cidades/", methods=["GET"])
 def get():
-    if request.method == "POST":
-        cidades = json.loads(urllib2.urlopen(api_url+"cidades/"+request.form['pais']).read())
+    if request.method == "GET":
+        cidades = json.loads(urllib2.urlopen(api_url+"cidades/"+request.args['pais']).read())
         return render_template("cidades.html", cidades = cidades['cidades'])
 
+
+@app.route("/addcidade")
+def addcidade():
+    paises = json.loads(urllib2.urlopen(api_url+"paises").read())
+    return render_template("addcidade.html", codes = paises['paises'])
+
+@app.route("/newcity/", methods=["POST"])
+def post():
+    if request.method == "POST":
+        dic = {
+            "Name"          : request.form['Name'],
+            "CountryCode"   : request.form['CountryCode'],
+            "District"      : request.form['District'],
+            "Population"    : request.form['Population'],
+        }
+        res = requests.post(api_url+"cidades", params=dic)
+        paises = json.loads(urllib2.urlopen(api_url+"paises").read())
+        return render_template("index.html", paises = paises['paises'])
+
+@app.route("/rmcidade")
+def rmcidade():
+    cidades = json.loads(urllib2.urlopen(api_url+"cidades").read())
+    return render_template("rmcidade.html", cidades = cidades['cidades'])
+
+@app.route("/delcity/", methods=["POST"])
+def delete():
+    if request.method == "POST":
+        res = requests.delete(api_url+"cidades/"+request.form["ID"])
+        paises = json.loads(urllib2.urlopen(api_url+"paises").read())
+        return render_template("index.html", paises = paises['paises'])
+
+@app.route("/attcidade")
+def attcidade():
+    return render_template("attcidade.html")
+
+@app.route("/upcity/", methods=["POST", "GET"])
+def path():
+    if request.method == "POST":
+        dic = {
+            "Name"          : request.form['Name'],
+            "CountryCode"   : request.form['CountryCode'],
+            "District"      : request.form['District'],
+            "Population"    : request.form['Population'],
+            "ID"            : request.form['ID'],
+        }
+        res = requests.patch(api_url+"cidades", params=dic)
+        paises = json.loads(urllib2.urlopen(api_url+"paises").read())
+        return render_template("index.html", paises = paises['paises'])
+
+
 if __name__ == '__main__':
-    app.run("127.0.0.1", port=3000, debug=True, )
+    app.run("127.0.0.1", port=2000, debug=True)
